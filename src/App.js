c854cec077a4e6763e1c05161f38be9e2a6ce6b3
LuisM <luism6977@gmail.com>
2021-03-15 10:57:08 -0300

    Test 4 only with basename

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { HashRouter as Router } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Practice } from './components/Practice';
 import { getCards } from './services/cardService';
 import { CardList } from './components/CardList';
@@ -33,7 +33,7 @@ function App() {
       </header>
       <main>
         <h3 className="app_main_title">Your Cards</h3>
-        <Router>
+        <Router basename="/flash-cards-app">
           <CardList
             path="/"
             cards={cards}
